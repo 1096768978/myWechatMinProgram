4e4cdcf8596c5a402c4a350181a79570aa7bf73a
luowenyuan <1096768978@qq.com>
2019-12-24 14:16:59 +0800

    第一次提交

---
+++ pages/Home/index.js
@@ -0,0 +1,32 @@
+// pages/Home/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+  
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgUrls: [
+      '/images/lunbo1.jpg',
+      '/images/lunbo2.jpg',
+      '/images/lunbo3.jpg',
+      '/images/lunbo4.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 500,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  }
+})
