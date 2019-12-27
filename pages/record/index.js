9bf7692ea6ddbcdc3e8c0381bb8246b81929ba69
1 <a1@yifanMac.local>
2019-12-27 20:57:41 +0800

    第四个页面

--- pages/record/index.js
+++ pages/record/index.js
@@ -11,13 +11,17 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    exit:function() {
+      wx.navigateTo({
+        url: '/pages/index/index',
+      })
+    },
   }
 })
