0b329a40c238d22836dd4d87ad360b02366bca07
luowenyuan <1096768978@qq.com>
2019-12-30 01:00:44 +0800

    第四个页面

--- pages/record/index.js
+++ pages/record/index.js
@@ -1,4 +1,5 @@
 // pages/record/index.js
+var time = require('../../utils/time.js');
 Component({
   /**
    * 组件的属性列表
@@ -11,9 +12,16 @@ Component({
    * 组件的初始数据
    */
   data: {
-    
+    getInput:'历史搜索记录和搜索时间',
+    history1:'没有记录',
+    history2:'没有记录',
+    history3:'没有记录',
+    history4:'没有记录',
+    date1:'',
+    date2:'',
+    date3:'',
+    date4:'',
   },
-
   /**
    * 组件的方法列表
    */
@@ -23,5 +31,46 @@ Component({
         url: '/pages/index/index',
       })
     },
+    search:function() {
+      var that = this
+      var sjc = Date.parse(new Date())/1000
+      if (this.data.getInput === 'js' || this.data.getInput === 'py' || this.data.getInput === 'c' || this.data.getInput==='算法'){
+        wx.navigateTo({
+          url: '/pages/books/index',
+        })
+        if(that.data.getInput === '没有记录'){
+        that.setData({
+          getInput: that.data.getInput
+        })
+      }else if(that.data.history1 === '没有记录'){
+        that.setData({
+          history1:that.data.getInput,
+          date1:time.formatTimeTwo(sjc,'Y/M/D h:m:s')
+        }),
+        console.log(date1)
+      }else if(that.data.history2 === '没有记录'){
+        that.setData({
+          history2:that.data.getInput,
+          date2:time.formatTimeTwo(sjc,'Y/M/D h:m:s')
+        })
+      }else if(that.data.history3 === '没有记录'){
+        that.setData({
+          history3:that.data.getInput,
+          date3:time.formatTimeTwo(sjc,'Y/M/D h:m:s')
+        })
+      }else if(that.data.history4 === '没有记录'){
+        that.setData({
+          history4:that.data.getInput,
+          date4:time.formatTimeTwo(sjc,'Y/M/D h:m:s')
+        })
+      }
+      }else{
+      wx.navigateTo({
+        url: '/pages/error/index',
+      })}
+    },
+    getInput:function(e) {
+      this.data.getInput= e.detail.value
+    }
   }
 })
