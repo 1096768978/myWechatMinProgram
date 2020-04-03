3890004b07987338d537e8a782abf971eaa4cad8
luozhixin <1096768978@qq.com>
2020-04-03 20:06:09 +0800

    fixbug

--- pages/bar/index.js
+++ pages/bar/index.js
@@ -3,136 +3,164 @@ import * as echarts from '../../ec-canvas/echarts';
 let chart = null;
 
 function initChart(canvas, width, height, dpr) {
-  chart = echarts.init(canvas, null, {
-    width: width,
-    height: height,
-    devicePixelRatio: dpr // new
-  });
-  canvas.setChart(chart);
+    chart = echarts.init(canvas, null, {
+        width: width,
+        height: height,
+        devicePixelRatio: dpr // new
+    });
+    canvas.setChart(chart);
 
-  var option = {
-    color: ['#37a2da', '#32c5e9', '#67e0e3'],
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-        type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-      },
-      confine: true
-    },
-    legend: {
-      data: ['总分', '得分', '丢分']
-    },
-    grid: {
-      left: 20,
-      right: 20,
-      bottom: 15,
-      top: 40,
-      containLabel: true
-    },
-    xAxis: [
-      {
-        type: 'value',
-        axisLine: {
-          lineStyle: {
-            color: '#999'
-          }
+    var option = {
+        color: ['#37a2da', '#32c5e9', '#67e0e3'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+            },
+            confine: true
         },
-        axisLabel: {
-          color: '#666'
-        }
-      }
-    ],
-    yAxis: [
-      {
-        type: 'category',
-        axisTick: { show: false },
-        data: ['Java', 'C++', '算法', 'JavaScript', 'Python'],
-        axisLine: {
-          lineStyle: {
-            color: '#999'
-          }
+        legend: {
+            data: ['总分', '得分', '丢分']
         },
-        axisLabel: {
-          color: '#666'
-        }
-      }
-    ],
-    series: [
-      {
-        name: '总分',
-        type: 'bar',
-        label: {
-          normal: {
-            show: true,
-            position: 'inside'
-          }
+        grid: {
+            left: 20,
+            right: 20,
+            bottom: 15,
+            top: 40,
+            containLabel: true
         },
-        data: [300, 270, 340, 344, 300],
-        itemStyle: {
-          // emphasis: {
-          //   color: '#37a2da'
-          // }
-        }
-      },
-      {
-        name: '得分',
-        type: 'bar',
-        stack: '总量',
-        label: {
-          normal: {
-            show: true
-          }
-        },
-        data: [280, 238, 219, 210, 210],
-        itemStyle: {
-          // emphasis: {
-          //   color: '#32c5e9'
-          // }
-        }
-      },
-      {
-        name: '丢分',
-        type: 'bar',
-        stack: '总量',
-        label: {
-          normal: {
-            show: true,
-            position: 'left'
-          }
-        },
-        data: [-20, -32, -21, -34, -90],
-        itemStyle: {
-          // emphasis: {
-          //   color: '#67e0e3'
-          // }
-        }
-      }
-    ]
-  };
-
-  chart.setOption(option);
-  return chart;
+        xAxis: [{
+            type: 'value',
+            axisLine: {
+                lineStyle: {
+                    color: '#999'
+                }
+            },
+            axisLabel: {
+                color: '#666'
+            }
+        }],
+        yAxis: [{
+            type: 'category',
+            axisTick: { show: false },
+            //data: ['Java', 'C++', '算法', 'JavaScript', 'Python'],
+            data: ['前端基础题'],
+            axisLine: {
+                lineStyle: {
+                    color: '#999'
+                }
+            },
+            axisLabel: {
+                color: '#666'
+            }
+        }],
+        series: [{
+                name: '总分',
+                type: 'bar',
+                label: {
+                    normal: {
+                        show: true,
+                        position: 'inside'
+                    }
+                },
+                //data: [300, 270, 340, 344, 300],
+                data: [25],
+                itemStyle: {
+                    // emphasis: {
+                    //   color: '#37a2da'
+                    // }
+                }
+            },
+            {
+                name: '得分',
+                type: 'bar',
+                stack: '总量',
+                label: {
+                    normal: {
+                        show: true
+                    }
+                },
+                //data: [280, 238, 219, 210, 210],
+                data: [3],
+                itemStyle: {
+                    // emphasis: {
+                    //   color: '#32c5e9'
+                    // }
+                }
+            },
+            {
+                name: '丢分',
+                type: 'bar',
+                stack: '总量',
+                label: {
+                    normal: {
+                        show: true,
+                        position: 'left'
+                    }
+                },
+                //data: [-20, -32, -21, -34, -90],
+                data: [-22],
+                itemStyle: {
+                    // emphasis: {
+                    //   color: '#67e0e3'
+                    // }
+                }
+            }
+        ]
+    };
+    chart.setOption(option);
+    return chart;
 }
 
 Page({
-  onShareAppMessage: function (res) {
-    return {
-      title: 'ECharts 可以在微信小程序中使用啦！',
-      path: '/pages/index/index',
-      success: function () { },
-      fail: function () { }
-    }
-  },
-  data: {
-    ec: {
-      onInit: initChart
-    }
-  },
+    data: {
+        errorcount: 0,
+        rightcount: 0,
+        count: 0,
+        sum: 0,
+        ec: {
+            onInit: initChart
+        }
+    },
+    onLoad: function(data) {
+        const eventChannel = this.getOpenerEventChannel()
+            // 监听acceptDataFromOpenerPage事件，获取上一页面通过eventChannel传送到当前页面的数据
+        eventChannel.on('acceptDataFromOpenerPage', (data) => {
+            this.data.count = data.count
+            this.data.errorcount = (data.errorcount * 2)
+            this.data.sum = (data.errorcount + data.rightcount) * 5
+                // chart.setOption({
+                //     series: [{
+                //             // 根据名字对应到相应的系列
+                //             name: '总分',
+                //             data: this.data.sum
+                //         },
+                //         {
+                //             name: '得分',
+                //             data: this.data.count
+                //         },
+                //         {
+                //             name: '总分',
+                //             data: this.data.errorcount
+                //         }
+                //     ]
+                // })
+            console.log(111, this.data)
+        })
+    },
+    onShareAppMessage: function(res) {
+        return {
+            title: 'ECharts 可以在微信小程序中使用啦！',
+            path: '/pages/index/index',
+            success: function() {},
+            fail: function() {}
+        }
+    },
 
-  onReady() {
-    setTimeout(function () {
-      // 获取 chart 实例的方式
-      // console.log(chart)
-    }, 2000);
-  }
+    onReady() {
+        setTimeout(function() {
+            // 获取 chart 实例的方式
+            // console.log(chart)
+        }, 2000);
+    }
 });
--- pages/practiseDemo/index.js
+++ pages/practiseDemo/index.js
@@ -1,194 +1,223 @@
 // pages/practiseDemo/index.js
 // pages/game/game.js
 var app = getApp();
+
 function two_char(n) {
-  return n >= 10 ? n : "0" + n;
+    return n >= 10 ? n : "0" + n;
 }
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    questions: ['设置元素浮动后，该元素的 display 值是多少( )', '以下不是 canvas 的⽅方法是( )', '不是 input 在 html5 新的类型的是( )', 'var temp=null，alert(typeof temp)弹出的结果是( )','以下对 Ajax 描述不正确的是( )'],
-    questionsA: ['A.block', 'A.getContext()', 'A.datetime', 'A.Null','A.readyState 属性请求的状态，当值为 3 时是正在加载'],
-    questionsB: ['B. 不变', 'B. fill()', 'B. file', 'B. Object','B.使用 XML 和 XSLT 进⾏行数据交换及相关操作'],
-    questionsC: ['C. inline', 'C. stroke()', 'C. color', 'C. Undefined','C.总共有 8 种 callback'],
-    questionsD: ['D. inline-block', 'D.controller', 'D. range', 'D. String','D.abort()⽅法，停⽌当前请求'],
-    answer: ['A','D','B','B','A'],
-    questionbody: '',
-    A: '',
-    B: '',
-    C: '',
-    D: '',
-    Ans: '',
-    show: '',
-    countj: true,
-    bindcount: 0,
-    errorcount: 0,
-    rightcount: 0,
-    count: 0,
-    time: '',
-    index: '',
-    transition:'',
-    transform:'',
-  },
-
-  /*设置计数器5分钟*/
-  onLoad: function (options) {
-    var sec = options.sec;
-    var that = this;
-    var si = setInterval(function () {
-      if (sec > 0) {
-        sec--;
-        var date = new Date(0, 0)
-        date.setSeconds(sec);
-        var h = date.getHours(), m = date.getMinutes(), s = date.getSeconds();
-        that.setData({
-          time: two_char(h) + ":" + two_char(m) + ":" + two_char(s)
-        })
-      }
-      else {
-        var count = that.data.count;
-        if (that.data.bindcount == 0) {
-          wx.showModal({
-            title: '提示：',
-            showCancel: false,
-            content: '开始作答请集中注意力',
-            success: function () {
-              wx.switchTab({
-                url: '',
-              })
-            }
-          })
-          clearInterval(si);
-        }
-        else {
-          clearInterval(si);
-          app.globalData.bindcount = that.data.bindcount;
-          app.globalData.errorcount = that.data.errorcount;
-          app.globalData.rightcount = that.data.rightcount;
-          wx.redirectTo({
-            url: '' + count,
-          })
-        }
-      }
-    }, 1000);
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    this.refresh();
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-  refresh: function () {
-    this.setData({
-      transform:this.data.transform = 'rotate(-180deg)',
-      transition: this.data.transition = 'all 0.5s'
-    })
-    var that = this;
-    var questions = that.data.questions;
-    var questionsA = that.data.questionsA;
-    var questionsB = that.data.questionsB;
-    var questionsC = that.data.questionsC;
-    var questionsD = that.data.questionsD;
-    var answer = that.data.answer;
-    var index = Math.floor(Math.random() * (questions.length - 1));
-    if (questions.length > 0) {
-      this.setData({
-        questionbody: questions.splice(index, 1),
-        A: questionsA.splice(index, 1),
-        B: questionsB.splice(index, 1),
-        C: questionsC.splice(index, 1),
-        D: questionsD.splice(index, 1),
-        Ans: answer.splice(index, 1),
-        questions: questions,
-        questionsA: questionsA,
-        questionsB: questionsB,
-        questionsC: questionsC,
-        questionsD: questionsD,
-        answer: answer,
-        index: index,
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        questions: ['设置元素浮动后，该元素的 display 值是多少( )',
+            '以下不是 canvas 的⽅方法是( )',
+            '不是 input 在 html5 新的类型的是( )',
+            'var temp=null，alert(typeof temp)弹出的结果是( )',
+            '以下对 Ajax 描述不正确的是( )',
+        ],
+        questionsA: ['A.block', 'A.getContext()', 'A.datetime', 'A.Null', 'A.readyState 属性请求的状态，当值为 3 时是正在加载'],
+        questionsB: ['B. 不变', 'B. fill()', 'B. file', 'B. Object', 'B.使用 XML 和 XSLT 进⾏行数据交换及相关操作'],
+        questionsC: ['C. inline', 'C. stroke()', 'C. color', 'C. Undefined', 'C.总共有 8 种 callback'],
+        questionsD: ['D. inline-block', 'D.controller', 'D. range', 'D. String', 'D.abort()⽅法，停⽌当前请求'],
+        answer: ['A', 'D', 'B', 'B', 'A'],
+        questionbody: '',
+        A: '',
+        B: '',
+        C: '',
+        D: '',
+        Ans: '',
         show: '',
         countj: true,
-      })
-    }
-    else {
-      wx.showModal({
-        title: '温馨提示',
-        content: '没题了',
-      })
-    }
-  },
-  disp: function (e) {
-    var id = e.currentTarget.id;
-    var num = e.currentTarget.dataset.num;
-    var count = e.currentTarget.dataset.count;
-    var bindcount = e.currentTarget.dataset.bindcount;
-    var rightcount = e.currentTarget.dataset.rightcount;
-    var errorcount = e.currentTarget.dataset.errorcount;
-    if (this.data.countj) {
-      if (id == num) {
+        bindcount: 0,
+        errorcount: 0,
+        rightcount: 0,
+        count: 0,
+        time: '',
+        index: '',
+        transition: '',
+        transform: '',
+    },
+
+    /*设置计数器5分钟*/
+    onLoad: function(options) {
+        var sec = options.sec;
+        var that = this;
+        var si = setInterval(function() {
+            if (sec > 0) {
+                sec--;
+                var date = new Date(0, 0)
+                date.setSeconds(sec);
+                var h = date.getHours(),
+                    m = date.getMinutes(),
+                    s = date.getSeconds();
+                that.setData({
+                    time: two_char(h) + ":" + two_char(m) + ":" + two_char(s)
+                })
+                console.log(111)
+            } else {
+                var count = that.data.count;
+                if (that.data.bindcount == 0) {
+                    wx.showModal({
+                        title: '提示：',
+                        showCancel: false,
+                        content: '开始作答请集中注意力',
+                        success: function() {
+                            wx.switchTab({
+                                url: '',
+                            })
+                        }
+                    })
+                    clearInterval(si);
+                } else {
+                    clearInterval(si);
+                    app.globalData.bindcount = that.data.bindcount;
+                    app.globalData.errorcount = that.data.errorcount;
+                    app.globalData.rightcount = that.data.rightcount;
+                    wx.redirectTo({
+                        url: '' + count,
+                    })
+                }
+            }
+        }, 1000);
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+        this.refresh();
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    },
+    disp: function(e) {
+        var id = e.currentTarget.id;
+        var num = e.currentTarget.dataset.num;
+        var count = e.currentTarget.dataset.count;
+        var bindcount = e.currentTarget.dataset.bindcount;
+        var rightcount = e.currentTarget.dataset.rightcount;
+        var errorcount = e.currentTarget.dataset.errorcount;
+        if (this.data.countj) {
+            if (id == num) {
+                this.setData({
+                    show: '正确！',
+                    count: count + 5,
+                    countj: false,
+                    bindcount: bindcount + 1,
+                    rightcount: rightcount + 1,
+                })
+            } else {
+                this.setData({
+                    show: '错误！' + '答案：' + num,
+                    count: count - 2,
+                    countj: false,
+                    bindcount: bindcount + 1,
+                    errorcount: errorcount + 1,
+                })
+            }
+        }
+    },
+    refresh: function() {
         this.setData({
-          show: '正确！',
-          count: count + 5,
-          countj: false,
-          bindcount: bindcount + 1,
-          rightcount: rightcount + 1,
+            transform: this.data.transform = 'rotate(-180deg)',
+            transition: this.data.transition = 'all 0.5s'
         })
-      }
-      else {
-        this.setData({
-          show: '错误！' + '答案：' + num,
-          count: count - 2,
-          countj: false,
-          bindcount: bindcount + 1,
-          errorcount: errorcount + 1,
-        })
-      }
-    }
-  }
+        var that = this;
+        var questions = that.data.questions;
+        var questionsA = that.data.questionsA;
+        var questionsB = that.data.questionsB;
+        var questionsC = that.data.questionsC;
+        var questionsD = that.data.questionsD;
+        var answer = that.data.answer;
+        var index = Math.floor(Math.random() * (questions.length - 1));
+        if (questions.length > 0) {
+            this.setData({
+                questionbody: questions.splice(index, 1),
+                A: questionsA.splice(index, 1),
+                B: questionsB.splice(index, 1),
+                C: questionsC.splice(index, 1),
+                D: questionsD.splice(index, 1),
+                Ans: answer.splice(index, 1),
+                questions: questions,
+                questionsA: questionsA,
+                questionsB: questionsB,
+                questionsC: questionsC,
+                questionsD: questionsD,
+                answer: answer,
+                index: index,
+                show: '',
+                countj: true,
+            })
+        } else {
+            let data = {
+                count: '',
+                rightcount: '',
+                errorcount: ''
+            }
+            data.count = this.data.count
+            data.rightcount = this.data.rightcount
+            data.errorcount = this.data.errorcount
+            console.log(data)
+            wx.showModal({
+                title: '温馨提示',
+                content: '题库已空～',
+                confirmText: '查看分数',
+                success(res) {
+                    if (res.confirm) {
+                        console.log('用户点击确定')
+                        wx.navigateTo({
+                            url: '/pages/bar/index',
+                            success: function(res) {
+                                // 通过eventChannel向被打开页面传送数据
+                                res.eventChannel.emit('acceptDataFromOpenerPage', data)
+                            }
+                        })
+                    } else if (res.cancel) {
+                        console.log('用户点击取消')
+                    }
+                }
+            })
+        }
+    },
 })
